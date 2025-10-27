b9f7d02722d5598e95494ca76c061d358587610e
surajnaithani <surajnathani4@gmail.com>
2025-10-27 14:53:39 +0530

    fix profileapi issue

--- client/src/redux/api/profileApi.js
+++ client/src/redux/api/profileApi.js
@@ -1,1 +1,44 @@
-localStorage.setItem('token', response.data.token);
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { server } from "../../constants/config";
+
+export const profileApi = createApi({
+  reducerPath: "profileApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${server}/api/v1/profile/`,
+    credentials: "include",
+  }),
+  tagTypes: ["users"],
+
+  endpoints: (builder) => ({
+    fetchUserProfile: builder.query({
+      query: () => ({
+        url: "me",
+        method: "GET",
+      }),
+      providesTags: ["users"],
+    }),
+
+    updateUserProfile: builder.mutation({
+      query: (updateUser) => ({
+        url: "updateProfile",
+        method: "PUT",
+        body: updateUser,
+      }),
+      invalidatesTags: ["users"],
+    }),
+
+    deleteUser: builder.mutation({
+      query: () => ({
+        url: `deleteProfile`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["users"],
+    }),
+  }),
+});
+
+export const {
+  useFetchUserProfileQuery,
+  useUpdateUserProfileMutation,
+  useDeleteUserMutation,
+} = profileApi;
